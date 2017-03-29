c9d06fd6a82302a852158de012b724003299eb9a
Gonzalo Martin <gonzalo.martin@globallogic.com>
2017-03-29 12:38:24 -0300

    Initial project

---
+++ app/src/main/kotlin/com.lalosoft.kotlinsample/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.lalosoft.kotlinsample
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.content_login.login_button
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        login_button.setOnClickListener { view ->
+            Toast.makeText(view.context, "Login", Toast.LENGTH_SHORT).show() }
+    }
+
+}
